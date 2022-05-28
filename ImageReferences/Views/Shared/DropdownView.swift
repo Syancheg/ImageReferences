cb0cbfafe613ff258368d3da6b36b6dc9618cdc0
Konstantin <syancheg@gmail.com>
2022-05-28 20:08:19 +0300

    add animate dropdown

--- ImageReferences/Views/Shared/DropdownView.swift
+++ ImageReferences/Views/Shared/DropdownView.swift
@@ -5,8 +5,11 @@
 //  Created by Константин Кузнецов on 21.05.2022.
 //
 
-import Foundation
 import UIKit
+
+protocol DropdownOutputProtocol: AnyObject {
+    
+}
 
 class DropdownView: UIView {
     
@@ -15,9 +18,10 @@ class DropdownView: UIView {
     private var filterGroup: String = ""
     private var filters: [Filter] = []
     private var delegate: MainOutputDelegate?
+    private var tableHeigth = 0.0
     private let paddingTop = 30.0
     private let buttonHeight = 50.0
-    private let heigthLine = 50.0
+    private let heigthLine = 45.0
     
     private var button: UIButton = {
         let button = UIButton()
@@ -39,8 +43,15 @@ class DropdownView: UIView {
         let table = UITableView()
         table.layer.cornerRadius = 5
         table.layer.borderWidth = 1
-        table.layer.borderColor = UIColor.dropdownTableBorder.cgColor
+        table.layer.borderColor = UIColor.dropdownTableBorders.cgColor
         return table
+    }()
+    
+    private var overlayView: UIView = {
+        let view = UIView()
+        view.layer.opacity = 0
+        view.backgroundColor = .black
+        return view
     }()
     
     // MARK: - Initializers
@@ -50,6 +61,7 @@ class DropdownView: UIView {
         table.delegate = self
         table.dataSource = self
         table.translatesAutoresizingMaskIntoConstraints = false
+        table.separatorStyle = .none
         setupButton()
         setupConstraints()
     }
@@ -60,6 +72,7 @@ class DropdownView: UIView {
         self.filterGroup = filterGroup
         self.filters = filters
         self.delegate = delegate
+        setupOverlay()
     }
     
     required init?(coder: NSCoder) {
@@ -86,20 +99,90 @@ class DropdownView: UIView {
         button.layer.shadowOpacity = 0
     }
     
+    private func setupOverlay() {
+        guard let superview = superview else { return }
+        overlayView.frame = CGRect(
+            x: -superview.frame.origin.x,
+            y: -superview.frame.origin.y,
+            width: UIScreen.main.bounds.width,
+            height: UIScreen.main.bounds.height
+        )
+        let tap = UITapGestureRecognizer(target: self, action: #selector(tapOverlay))
+        overlayView.addGestureRecognizer(tap)
+    }
+    
+    private func viewOverlay() {
+        if overlayView.frame.height == 0.0 {
+            setupOverlay()
+        }
+        guard let superview = superview else { return }
+        superview.addSubview(overlayView)
+        UIView.animate(withDuration: 0.3) {
+            self.overlayView.layer.opacity = 0.3
+        }
+    }
+    
+    private func deleteOverlay() {
+        if overlayView.superview != nil {
+            UIView.animate(
+                withDuration: 0.3) {
+                    self.overlayView.layer.opacity = 0.0
+                } completion: { stop in
+                    self.overlayView.removeFromSuperview()
+                }
+        }
+    }
+    
     @objc private func buttonAction() {
-        if (table.superview != nil) {
-            table.removeFromSuperview()
-            buttonUnActive()
+        if table.superview != nil {
+            closeAnimation()
             return
         }
         buttonActive()
-        var y = frame.origin.y + frame.height
-        let height = Double(filters.count) * heigthLine
-        if (frame.origin.y > 150) {
-            y = frame.origin.y - height
+        openAnimation()
+    }
+    
+    @objc func tapOverlay() {
+        closeAnimation()
+    }
+    
+    // MARK: - Animations
+    
+    private func openAnimation() {
+        guard let superview = superview else { return }
+        var y = frame.origin.y
+        tableHeigth = Double(filters.count) * heigthLine
+        if frame.origin.y < 150 {
+            y += frame.height
+            table.frame = CGRect(x: 0, y: y, width: bounds.width, height: 0)
+        } else {
+            y += paddingTop - 7
+            table.frame = CGRect(x: 0, y: y, width: bounds.width, height: 0)
+            y -= self.tableHeigth
+            
         }
-        table.frame = CGRect(x: 0, y: y, width: bounds.width, height: height)
-        superview?.addSubview(table)
+        viewOverlay()
+        superview.addSubview(table)
+        UIView.animate(withDuration: 0.3) {
+            self.table.frame = CGRect(x: 0, y: y, width: self.bounds.width, height: self.tableHeigth)
+        }
+        
+    }
+    
+    func closeAnimation() {
+        var y = frame.origin.y
+        if frame.origin.y < 150 {
+            y += frame.height
+        } else {
+            y += paddingTop - 7
+        }
+        deleteOverlay()
+        UIView.animate(withDuration: 0.3) {
+            self.table.frame = CGRect(x: 0, y: y, width: self.bounds.width, height: 0)
+        } completion: { stop in
+            self.table.removeFromSuperview()
+            self.buttonUnActive()
+        }
     }
     
     // MARK: - Setup Constraints
@@ -121,15 +204,17 @@ extension DropdownView: UITableViewDelegate, UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = DropdownTableViewCell()
         cell.textLabel?.text = filters[indexPath.row].name
+        if indexPath.row != filters.count - 1 {
+            cell.setupSeparator()
+        }
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         button.setTitle(filters[indexPath.row].name, for: .normal)
-        buttonUnActive()
-        table.removeFromSuperview()
+        closeAnimation()
         guard let delegate = delegate else { return }
         delegate.setFilter(with: [filterGroup: filters[indexPath.row].id])
     }
